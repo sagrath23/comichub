2471004052553b997518daed16b39a42e8cbce92
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-02-05 01:08:00 -0500

    improve distribution at ComicDetail

--- src/components/Comic/ComicDetail/ComicDetail.js
+++ src/components/Comic/ComicDetail/ComicDetail.js
@@ -4,6 +4,7 @@ import { useHistory, useParams } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import { comicRequest } from '../../../store/domains/comic';
 import { extractComicFullname } from '../../../utils';
+import './ComicDetail.css';
 
 const IssueDetail = () => {
   const dispatch = useDispatch();
@@ -26,34 +27,41 @@ const IssueDetail = () => {
   }, [dispatch, issueID]);
 
   return (
-    <div>
-      <h2 data-testid="pokemon-name">{extractComicFullname(issue)}</h2>
-      <img className="img-fluid" alt="volume_cover" src={coverUrl} />
-      <div className="detail-item">
+    <div className="detail-container">
+      <div className="title">
+        <h2 data-testid="pokemon-name">{extractComicFullname(issue)}</h2>
+      </div>
+      <div className="cover">
+        <img className="img-fluid" alt="volume_cover" src={coverUrl} />
+      </div>
+      
+      <div className="characters">
         <h3>Characters</h3>
         {characterList.map((character) => (
           <div>{character.name}</div>
         ))}
       </div>
-      <div className="detail-item">
+      <div className="teams">
         <h3>Teams</h3>
         {teamList.map((team) => (
           <div>{team.name}</div>
         ))}
       </div>
-      <div className="detail-item">
+      <div className="locations">
         <h3>Locations</h3>
         {locationList.map((location) => (
           <div>{location.name}</div>
         ))}
       </div>
-      <div className="detail-item">
+      <div className="concepts">
         <h3>Concepts</h3>
         {conceptList.map((concept) => (
           <div>{concept.name}</div>
         ))}
       </div>
-      <Button color="primary" onClick={handleClick}>primary</Button>
+      <div className="nav-btns">
+        <Button color="primary" onClick={handleClick}>Go back</Button>
+      </div>
     </div>
   );
 };
